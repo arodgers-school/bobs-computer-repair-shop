c8bde7ffec9f057ee3e8c24c883f2f7a7d9769ca
Adam School <adrodgers@my365.bellevue.edu>
2022-03-02 18:32:30 -0600

    WEB425 Week 9 updates

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,3 +1,6 @@
+//
+// References:  https://blog.angular-university.io/angular-2-ngfor/
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,15 +11,53 @@ import { Component, OnInit } from '@angular/core';
 export class MainComponent implements OnInit {
   constructor() {}
 
-  typesOfServices: string[] = [
-    'Password Reset ($39.99)',
-    'Spyware Removal ($99.99)',
-    'RAM Upgrade ($129.99)',
-    'Software Installation ($49.99)',
-    'Tune-up ($89.99)',
-    'Keyboard Cleaning ($45.00)',
-    'Disk Clean-up ($149.99)',
+  submit() {
+    console.log(this.selectedServices);
+  }
+
+  typesOfServices: {
+    id: string;
+    name: string;
+    price: number;
+  }[] = [
+    {
+      id: 'passwordReset',
+      name: 'Password Reset',
+      price: 39.99,
+    },
+    {
+      id: 'spywareRemoval',
+      name: 'Spyware Removal',
+      price: 99.99,
+    },
+    {
+      id: 'ramUpgrade',
+      name: 'RAM Upgrade',
+      price: 129.99,
+    },
+    {
+      id: 'softwareInstall',
+      name: 'Software Installation',
+      price: 49.99,
+    },
+    {
+      id: 'tuneUp',
+      name: 'Tune-up',
+      price: 89.99,
+    },
+    {
+      id: 'keyboardCleaning',
+      name: 'Keyboard Cleaning',
+      price: 45.0,
+    },
+    {
+      id: 'diskCleanUp',
+      name: 'Disk Clean-up',
+      price: 149.99,
+    },
   ];
+
+  selectedServices: string[];
 
   ngOnInit(): void {}
 }
