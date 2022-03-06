964dbc56a449d5f46967f998162cb455034c46aa
Adam School <adrodgers@my365.bellevue.edu>
2022-03-06 12:53:04 -0600

    Week 9 Updates

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,26 @@
+/*
+Title: 
+    Assignment 9.3
+Author: 
+    Adam Rodgers
+Date: 
+    3/2/2022
+Modified By: Adam Rodgers
+Description: Bob's Computer Repair Shop
+Resources:
+    Bellevue University WEB425 Github Repo
+    https://material.angular.io/components/categories
+*/
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
